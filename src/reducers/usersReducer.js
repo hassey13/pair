86c7dfbcda13d1f3840d76d945b7448a7c1a71e7
Mohammed Chisti <Chisti@Mohammeds-MacBook-Air.local>
2017-02-14 17:08:48 -0500

    API and React User post and get calls

--- src/reducers/usersReducer.js
+++ src/reducers/usersReducer.js
@@ -1,9 +1,9 @@
 export default function(state=[], action){
   switch (action.type) {
-    case 'CREATE_USERS':
-      return action.payload.data.first_name
+    case 'CREATE_USER':
+      return action.payload.data.user
     case 'LOGIN_USER':
-      return action.payload.data
+      return action.payload.data.user
     default:
       return state
   }
