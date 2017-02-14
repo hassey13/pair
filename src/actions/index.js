13c85fc8e5d76cb0b1c0be5cf2acf22698dab7b1
limerentfox <christi@limerentfox.com>
2017-02-14 15:12:36 -0500

    Login Added. Components not done.

--- src/actions/index.js
+++ src/actions/index.js
@@ -5,14 +5,29 @@ const URL = 'http://localhost:4000/api/v1/'
 
 export const createUser = (user) => {
   const response = axios.post(URL + 'signup', user).then((data) => {
-    sessionStorage.setItem('jwt',response.data.jwt)
-    // browserHistory.push("/users")
+    sessionStorage.setItem('jwt', data.jwt)
+    browserHistory.push("/users")
     debugger
-    return response
+    return data
   })
 
   return {
     type: 'CREATE_USER',
+    payload: response
+  }
+}
+
+export const loginUser = (user) => {
+  const response = axios.post(URL + 'login', user).then((response) => {
+    sessionStorage.setItem('jwt', response.data.jwt)
+    browserHistory.push("/users")
+    console.log(response)
+
+    return response
+  })
+
+  return {
+    type: 'LOGIN_USER',
     payload: response
   }
 }
--- src/components/App.js
+++ src/components/App.js
@@ -11,15 +11,6 @@ export default class App extends Component {
         <div className='container'>
           {this.props.children}
         </div>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Tu autem negas fortem esse quemquam posse, qui dolorem malum putet. Cur id non ita fit? His enim rebus detractis negat se reperire in asotorum vita quod reprehendat. Quod ea non occurrentia fingunt, vincunt Aristonem; Equidem etiam Epicurum, in physicis quidem, Democriteum puto. Idemque diviserunt naturam hominis in animum et corpus. Duo Reges: constructio interrete. Quamquam te quidem video minime esse deterritum. Huius ego nunc auctoritatem sequens idem faciam. Ergo ita: non posse honeste vivi, nisi honeste vivatur? </p>
-
-        <p>Quid enim me prohiberet Epicureum esse, si probarem, quae ille diceret? Traditur, inquit, ab Epicuro ratio neglegendi doloris. Tum Torquatus: Prorsus, inquit, assentior; Maximus dolor, inquit, brevis est. Sed id ne cogitari quidem potest quale sit, ut non repugnet ipsum sibi. </p>
-
-        <p>Si autem id non concedatur, non continuo vita beata tollitur. Cur fortior sit, si illud, quod tute concedis, asperum et vix ferendum putabit? At ille pellit, qui permulcet sensum voluptate. Nam illud vehementer repugnat, eundem beatum esse et multis malis oppressum. Et quidem Arcesilas tuus, etsi fuit in disserendo pertinacior, tamen noster fuit; Mihi enim satis est, ipsis non satis. Eam si varietatem diceres, intellegerem, ut etiam non dicente te intellego; Sed haec quidem liberius ab eo dicuntur et saepius. </p>
-
-        <p>Ut aliquid scire se gaudeant? Suo enim quisque studio maxime ducitur. Hoc enim constituto in philosophia constituta sunt omnia. An dolor longissimus quisque miserrimus, voluptatem non optabiliorem diuturnitas facit? Potius inflammat, ut coercendi magis quam dedocendi esse videantur. Illa tamen simplicia, vestra versuta. Quae in controversiam veniunt, de iis, si placet, disseramus. </p>
-
-        <p>Sed venio ad inconstantiae crimen, ne saepius dicas me aberrare; Haeret in salebra. Quae autem natura suae primae institutionis oblita est? Haec bene dicuntur, nec ego repugno, sed inter sese ipsa pugnant. Hic ambiguo ludimur. Age nunc isti doceant, vel tu potius quis enim ista melius? Itaque hic ipse iam pridem est reiectus; </p>
       </div>
     );
   }
