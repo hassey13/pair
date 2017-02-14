cd5fc4b650722ac38ce8bc9e69a65a122f85351e
limerentfox <christi@limerentfox.com>
2017-02-14 17:19:21 -0500

    Added comment and profile

---
+++ src/components/Profile.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+import {connect} from 'react-redux'
+
+export class Profile extends Component {
+  render() {
+    return <div />
+  }
+}
+
+export const mapStateToProps = (state) => {
+  return {
+
+  }
+}
+
+export const mapDispatchToProps = {
+
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Profile)
