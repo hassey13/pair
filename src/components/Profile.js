7ad4a9c37a79cd6c5ef994ef9e95373967167d34
limerentfox <christi@limerentfox.com>
2017-02-14 20:38:53 -0500

    spec

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -6,12 +6,20 @@ export class Profile extends Component {
     return (
       <div className='profile'>
 
-        <div className='profile__name'>
-
+        <div className='sidepanel'>
+          <div className='sidepanel__avatar'></div>
+          <p className='sidepanel__fullName'>Full Name</p>
+          <p className='sidepanel__username'>Username</p>
+          <Link className='sidepanel__followers'>Followers</Link>
+          <Link className='sidepanel__following'>Following</Link>
+          <p className='sidepanel__bio'>WAKAWAKAWAKAWAKAWAKA</p>
+          <div className='iconRow'>
+            <div className='iconRow__icon'></div>
+          </div>
         </div>
 
 
-        
+
       </div>
     )
   }
