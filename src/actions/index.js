2d49cb3fc3588810d31681669edf3f5ccf4c8aea
hassey13 <ehasselbring@gmail.com>
2017-02-15 12:06:20 -0500

    api calls for stock cards

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,6 @@
 import axios from 'axios'
 import { browserHistory } from 'react-router'
+import { ApiKey } from '../../secret/ApiKey'
 
 const URL = 'http://localhost:4000/api/v1/'
 
@@ -30,10 +31,28 @@ export const loginUser = (user) => {
 }
 
 export const fetchStocksOwned = () => {
-  const response = axios.get('http://localhost:4000/api/v1/stocks').then(response => response.data)
+  const response = axios.get(URL + 'stocks').then(response => response.data)
 
   return {
     type: 'FETCH_OWNED_STOCKS',
     payload: response
   }
 }
+
+// export const fetchStockCardData = (ticker) => {
+//
+//   var config = { headers: { Authorization: 'Basic ' + btoa(ApiKey.password) }}
+//
+//   var baseURL = "https://api.intrinio.com/prices?ticker="
+//   var timePeriod = '&item=close_price&start_date=2017-02-13&end_date=2017-02-14'
+//   var URL = baseURL + ticker + timePeriod
+//
+//   const response = axios.get(URL, config).then(response => response.data)
+//
+//   console.log(response)
+//
+//   return {
+//     type: 'FETCH_STOCK_CARD_DATA',
+//     payload: response
+//   }
+// }
--- src/reducers/stocksReducer.js
+++ src/reducers/stocksReducer.js
@@ -1,7 +1,15 @@
 export default function(state=[], action){
   switch (action.type) {
     case 'FETCH_OWNED_STOCKS':
-      return action.payload
+      return {
+        symbols: action.payload,
+        data: state.data
+      }
+    case 'FETCH_STOCK_CARD_DATA':
+      return {
+        symbols: state.symbols,
+        data: [...state.data, action.payload]
+      }
     default:
       return state
   }
