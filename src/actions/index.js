0e149453bd928a0ef0ae57e6c1a9cd8b111652e6
Mohammed Chisti <Chisti@Mohammeds-MacBook-Air.local>
2017-02-15 12:04:23 -0500

    Stock Adapter created

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,8 +1,9 @@
 import { browserHistory } from 'react-router'
 import { userAdapter } from '../adapters/UsersAdapter'
+import { stockAdapter } from '../adapters/StockAdapter'
 
 export const createUser = (user) => {
-  
+
   const response = userAdapter.createUser(user)
 
   return {
@@ -21,7 +22,7 @@ export const loginUser = (user) => {
 }
 
 export const fetchStocksOwned = () => {
-  const response = axios.get('http://localhost:4000/api/v1/stocks').then(response => response.data)
+  const response = stockAdapter.fetchStock()
 
   return {
     type: 'FETCH_OWNED_STOCKS',
