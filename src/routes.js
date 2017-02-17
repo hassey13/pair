3381611893f748e5e52178a3ccc4638a42fbbad9
Mohammed Chisti <Chisti@Mohammeds-MacBook-Air.local>
2017-02-16 21:24:16 -0500

    Style changes to Search. Search Results now redirect to a stock show page. Added Stock page.

--- src/routes.js
+++ src/routes.js
@@ -1,18 +1,15 @@
-import {IndexRoute, Route} from 'react-router'
+import React from 'react'
+import { IndexRoute, Route } from 'react-router'
 
 import App from './components/App'
 import CommentForm from './components/CommentForm.js'
 import CommentShow from './components/CommentShow.js'
 import Login from './components/Login'
 import Profile from './components/Profile'
-
-import React from 'react'
 import StockList from './components/stocks/StockList'
+import Stock from './components/stocks/Stock'
 import UserSignUp from './components/UserSignUp'
-
 import Edit from './components/Edit'
-
-
 
 export default (
   <Route path="/" component={ App }>
@@ -20,6 +17,7 @@ export default (
     <Route path="login" component={ Login } />
     <Route path="signup" component={ UserSignUp } />
     <Route path="stocks" component={ StockList } />
+    <Route path="stock" component={ Stock } />
     <Route path="profile" component={ Profile } />
     <Route path="newcomment" component={ CommentForm } />
     <Route path="comments" component={ CommentShow } />
