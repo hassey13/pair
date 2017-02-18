b51f6e267b457abaa9fb23872c026ea268a15fa8
limerentfox <christi@limerentfox.com>
2017-02-18 15:10:48 -0500

    clicking stocks

--- src/components/stocks/StockCard.js
+++ src/components/stocks/StockCard.js
@@ -1,39 +1,48 @@
 import React, { Component } from 'react'
 // import UnfollowStockButton from './UnfollowStockButton'
+import { browserHistory } from 'react-router'
 import {connect} from 'react-redux'
 import { unfollowStock } from '../../actions/index'
 import {Link} from 'react-router'
 
 class StockCard extends Component {
+  constructor(props) {
+    super(props)
+    this.handleClick = this.handleClick.bind(this)
+  }
+
 
   handleSubmit(event) {
     event.preventDefault()
     this.props.unfollowStock({ticker: this.props.stock.ticker})
   }
 
-  // handleClick(stockTicker){
-  //   this.props.updateCurrentStock(stockTicker)
-  // }
+  handleClick(event){
+    browserHistory.push({
+      pathname: `/stock/${this.props.stock.ticker}`
+    })
+  }
+
 
 
   render() {
     return (
 
-        <div className="card">
+        <div className="card" onClick={this.handleClick}>
           <form onSubmit={ this.handleSubmit.bind(this) }>
             <button type="submit" className="btn-clear">x</button>
           </form>
 
 
           <div className="card-text">
-            <Link to={`/stock/${this.props.stock.ticker}`}>
-              <div>
-                <h5>{ this.props.stock.ticker }</h5>
-                <p>{ this.props.stock.company_name }</p>
-                <img src="graph.png" alt="graph" className="graph" />
-                <p>{ this.props.stock.data[0].last_price }</p>
-              </div>
-            </Link>
+
+            <div>
+              <h5>{ this.props.stock.ticker }</h5>
+              <p>{ this.props.stock.company_name }</p>
+              <img src="graph.png" alt="graph" className="graph" />
+              <p>{ this.props.stock.data[0].last_price }</p>
+            </div>
+            
             </div>
 
           </div>
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,7 +3,6 @@ import usersReducer from './usersReducer'
 import stocksReducer from './stocksReducer'
 import commentsReducer from './commentsReducer'
 import searchReducer from './searchReducer'
-import currentStockReducer from './currentStockReducer'
 import sessionReducer from './sessionReducer'
 
 const rootReducer = combineReducers({
@@ -11,7 +10,6 @@ const rootReducer = combineReducers({
   stocks: stocksReducer,
   comments: commentsReducer,
   search: searchReducer,
-  currentStock: currentStockReducer
   session: sessionReducer
 })
 
